0837704225b611188556b96365ca08242f43989a
Zilleyy <coopercowley2004@gmail.com>
2021-05-08 22:04:13 +1000

    Fixed stuff lol.

--- src/main/java/com/zilleyy/wordcomp/Main.java
+++ src/main/java/com/zilleyy/wordcomp/Main.java
@@ -6,33 +6,34 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         RandomCollection<Word> words = new RandomCollection<>();
-        List<String> list  = new ArrayList<>();
-        try {
-            String jsonStr = Files.lines(Paths.get("src/main/java/com/zilleyy/wordcomp/random.json")).collect(Collectors.joining(System.lineSeparator()));
-            JSONArray json = new JSONObject(jsonStr).getJSONArray("data");
-            json.forEach(key -> {
-                String spelling = (String) key;
 
-                list.add(spelling);
+        try {
+            String jsonStr = Files.lines(Paths.get("src/main/java/com/zilleyy/wordcomp/words.json")).collect(Collectors.joining(System.lineSeparator()));
+            JSONObject json = new JSONObject(jsonStr);
+            json.keySet().forEach(key -> {
+                JSONArray array = json.getJSONArray(key);
+
+                String spelling = key;
+                double weight = array.getDouble(0);
+                int rank = array.getInt(1);
+
+                words.add(weight, new Word(spelling, weight, rank));
             });
         } catch (IOException exception) {
             exception.printStackTrace();
         }
 
-        String a = "";
-        for (byte index = 0; index < 100; index++) {
-            a += " " + list.get(new Random().nextInt(list.size()));
+        String result = "";
+        for (int index = 0; index < 100; index++) {
+            result += " " + words.next().getSpelling();
         }
-        System.out.println(a);
+        System.out.println(result);
     }
 
 }
